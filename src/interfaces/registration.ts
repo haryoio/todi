fd920ddc7a874c1e1248de17fd7cde4c5860bbf5
haryoiro <mizusecocolte@gmail.com>
2023-03-31 07:26:23 +0900

    Add(Injectable) InjectableOptionを追加

--- src/interfaces/registration.ts
+++ src/interfaces/registration.ts
@@ -1,5 +1,6 @@
 import { Provider } from './provider.ts';
 import { Lifetime } from './lifetime.ts';
+import { Token } from "./token.ts";
 
 export interface Registration<T> {
     provider: Provider<T>,
@@ -9,4 +10,8 @@ export interface Registration<T> {
 
 export interface RegistrationOptions {
     lifetime?: Lifetime;
+}
+
+export interface InjectableOptions extends RegistrationOptions {
+    token?: Token<any>
 }
