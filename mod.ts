b65a9187c4a80e972516b234da3ed835c09d1522
haryoiro <mizusecocolte@gmail.com>
2023-03-31 15:44:45 +0900

    WIP

--- mod.ts
+++ mod.ts
@@ -1,28 +1,8 @@
-import { Container, Disposable } from "./src/interfaces/container.ts"
-import { RegistrationOptions, Registration } from "./src/interfaces/registration.ts";
-import { Constructor, Token } from "./src/interfaces/token.ts";
-import { Registry } from "./src/registry.ts";
-import { InternalContainer, container, createContainer } from "./src/container.ts";
-import { Injectable, Inject, GlobalRegister, Singleton, Register } from "./src/decorator.ts";
-import { Provider } from "./src/interfaces/provider.ts";
 
-export {
-    Registry,
-    InternalContainer,
-    Injectable,
-    Inject,
-    GlobalRegister,
-    Singleton,
-    container,
-    createContainer,
-    Register,
-};
 
-export type {
-    Constructor,
-    Provider,
-    Token,
-    Container,
-    RegistrationOptions,
-    Registration
-};
+export * from "./src/interfaces/container.ts"
+export * from "./src/provider.ts"
+export * from "./src/container.ts"
+export * from "./src/decorator.ts"
+export * from "./src/registry.ts"
+export * from "./src/reflect.ts"
