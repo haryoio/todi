19dc0b09050000971498c87e3af2ea6a27ede7c2
haryoiro <mizusecocolte@gmail.com>
2023-03-31 14:45:51 +0900

    📃 docs(examples): 例を追加

---
+++ examples/dip_logger.ts
@@ -0,0 +1,27 @@
+import { container, Injectable, Inject } from "../mod.ts"
+
+
+interface Logger {
+    log(message: string): void;
+}
+
+class ConsoleLogger implements Logger {
+    log(message: string): void {
+        console.log(message);
+    }
+}
+
+@Injectable()
+class App {
+    constructor(
+        @Inject("Logger") private logger: Logger
+    ) { }
+
+    run(): void {
+        this.logger.log("Hello, World!");
+    }
+}
+
+container.register("Logger", { useClass: ConsoleLogger })
+const app = container.resolve(App);
+app.run();
