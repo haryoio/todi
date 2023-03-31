5cf21ef54204f2585cac65f42b3a273c0836b48f
haryoiro <mizusecocolte@gmail.com>
2023-03-31 11:55:36 +0900

    🧪 test(Container): シングルトン,依存関係の上書きへのテスト

--- tests/container.test.ts
+++ tests/container.test.ts
@@ -1,6 +1,6 @@
 import { assertEquals, assertThrows } from "../deps.ts";
 import { InternalContainer } from "../src/container.ts"
-import { Injectable, Inject } from "../src/decorator.ts"
+import { Injectable, Inject, Singleton } from "../src/decorator.ts"
 import { Token } from "../src/interfaces/token.ts";
 
 Deno.test("DI Container: Register and resolve class provider", () => {
@@ -49,11 +49,40 @@ Deno.test("DI Container: Register and resolve token provider", () => {
 
     const token: Token<Test> = "test token"
 
-    container.register<Test>(Test, { useClass: Test })
     container.register<Test>(token, { useToken: Test })
 
     const resolvedInstance = container.resolve<Test>(token)
     assertEquals(resolvedInstance.value, "test")
+})
+
+Deno.test("DI Container: Register duplicate token Override", () => {
+    const container = new InternalContainer();
+
+    interface ITest {
+        echo(value: string): string
+    }
+
+    @Injectable()
+    class Test implements ITest {
+        v = "test"
+        echo(value: string) {
+            return value + this.v
+        }
+    }
+
+    @Injectable()
+    class Test2 implements ITest {
+        v = "test2"
+        echo(value: string) {
+            return value + this.v
+        }
+    }
+
+    container.register<ITest>("Test", { useClass: Test });
+    container.register<ITest>("Test", { useClass: Test2 });
+
+    const testInstance = container.resolve<ITest>("Test");
+    assertEquals(testInstance.echo("!"), "!test2");
 })
 
 Deno.test("DI Container: RegisterAll and resolve", () => {
@@ -64,6 +93,42 @@ Deno.test("DI Container: Register and resolveAll class provider", () => { })
 
 Deno.test("DI Container: Test transient lifetime", () => { })
 
-Deno.test("DI Container: Test singleton lifetime", () => { })
+Deno.test("DI Container: Test singleton lifetime", () => {
+    const container = new InternalContainer();
+
+    @Singleton()
+    class SingletonTest {
+        value = "test"
+    }
+
+    container.register<SingletonTest>("Test", { useClass: SingletonTest })
+
+    const testInstance1 = container.resolve<SingletonTest>("Test");
+    const testInstance2 = container.resolve<SingletonTest>("Test");
+    assertEquals(testInstance1 === testInstance2, true);
+    assertEquals(testInstance1.value, "test");
+})
+
+Deno.test("DI Container: Test singleton and override item", () => {
+    const container = new InternalContainer();
+
+    @Singleton()
+    class SingletonTest {
+        value = "test"
+    }
+
+    @Singleton()
+    class SingletonTest2 {
+        value = "test2"
+    }
+
+    container.register<SingletonTest>("Test", { useClass: SingletonTest })
+    container.register<SingletonTest>("Test", { useClass: SingletonTest2 })
+
+    const testInstance1 = container.resolve<SingletonTest>("Test");
+    const testInstance2 = container.resolve<SingletonTest>("Test");
+    assertEquals(testInstance1 === testInstance2, true);
+    assertEquals(testInstance1.value, "test2");
+})
 
 Deno.test("DI Container: Test scoped lifetime", () => { })
